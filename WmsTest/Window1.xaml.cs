3728428ba1d68a147cb73a0a00cb1149c3a6095a
oliverheilig <oliver.heilig@yahoo.de>
2014-12-18 14:17:03 +0100

    fixes for basic auth and WMS, added WMS sample

---
+++ WmsTest/Window1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ptv.XServer.Controls.Map.Gadgets;
+using Ptv.XServer.Controls.Map.Layers.Shapes;
+using Ptv.XServer.Controls.Map;
+using System.Printing;
+using System.IO;
+using System.Windows.Media.Animation;
+using Ptv.XServer.Controls.Map.Layers.Tiled;
+using Ptv.XServer.Controls.Map.Symbols;
+using Ptv.XServer.Controls.Map.Layers.Untiled;
+using Ptv.XServer.Controls.Map.TileProviders;
+using Ptv.XServer.Controls.Map.Tools;
+
+namespace Circles
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            this.Map.Loaded += new RoutedEventHandler(Map_Loaded);
+        }
+
+        private void Map_Loaded(object sender, RoutedEventArgs e)
+        {
+            // http://ows.terrestris.de/dienste.html
+            Map.Layers.Add(new TiledLayer("BASEMAP")
+            {
+                Caption = "Base Map",
+                Icon = ResourceHelper.LoadBitmapFromResource("Ptv.XServer.Controls.Map;component/Resources/Background.png"),
+                TiledProvider = new WmsProvider(
+                    "http://ows.terrestris.de/osm/service?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2fpng&LAYERS=OSM-WMS&STYLES=,&SRS=EPSG%3a3857", 19),
+                Copyright = "© terrestris",
+                IsBaseMapLayer = true // overlay layers cannot be moved under a basemap layer
+            });
+            
+
+            Map.Layers.Add(new UntiledLayer("BUSSTOPS") { 
+                Caption = "Bus Stops",
+                UntiledProvider = new WmsUntiledProvider(
+                    "http://ows.terrestris.de/osm-haltestellen?LAYERS=OSM-Bushaltestellen&TRANSPARENT=true&FORMAT=image%2Fpng&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&STYLES=&SRS=EPSG%3A900913", 19),
+                Copyright = "© terrestris"
+
+            });
+        }
+    }
+}
