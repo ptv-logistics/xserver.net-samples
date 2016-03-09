c3ae053b060c0bd2f407fe98e0ac094ce1603c4b
oliverheilig <oliver.heilig@yahoo.de>
2016-03-09 10:52:58 +0100

    show direct initialization for CustomInit

--- CustomInit/Window1.xaml.cs
+++ CustomInit/Window1.xaml.cs
@@ -4,9 +4,8 @@ using Ptv.XServer.Controls.Map.Layers.Untiled;
 using Ptv.XServer.Controls.Map.Localization;
 using Ptv.XServer.Controls.Map.TileProviders;
 using Ptv.XServer.Controls.Map.Tools;
-using System.Security.Cryptography.X509Certificates;
-using System.Windows;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CustomInit
 {
@@ -20,44 +19,43 @@ namespace CustomInit
         {
             InitializeComponent();
 
-            InitializeMap();
+            string url = "https://xmap-eu-n-test.cloud.ptvgroup.com/xmap/ws/XMap";
+            string token = "678890783139995"; // just a test-token here. Use your own token
+
+            // v1: Use Meta info
+            InitializeMap(Map1, url, token);
+
+            // v2: direct initialization
+            InsertXMapBaseLayers(Map2.Layers, url, "PTV, TomTom", new Size(3840, 2400), "xtok", token);
         }
 
         /// <summary>
         /// Asynchronous initialization of the map
         /// </summary>
-        private async void InitializeMap()
+        private async void InitializeMap(WpfMap map, string url, string token)
         {
-            // Get meta info in separate task and await
-            var meta = await Task.Run(() => GetMetaInfo());
-
-            // Initialize the map
-            InsertXMapBaseLayers(Map.Layers, meta);
-        }
-
-        /// <summary>
-        /// Get the meta info from xServer
-        /// </summary>
-        /// <returns></returns>
-        private XMapMetaInfo GetMetaInfo()
-        {            
             // the tools class XMapMetaInfo contains the information required to intialize the xServer layers
             // When instantiated with the url, it tries to read the attribution text and the maximum request size from the xMap configuration
-            // var meta = new XMapMetaInfo("http://127.0.0.1:50010/xmap/ws/XMap"); // custom xmap with reverse proxy
+            // To avoid blocking the application init, call it asynchronously
+            var meta = await Task.Run(() => new XMapMetaInfo(url));
+            meta.SetCredentials("xtok", token); // set the basic authentication properties, e.g. xtok/token for xserver internet
 
-            var meta = new XMapMetaInfo("https://xmap-eu-n-test.cloud.ptvgroup.com/xmap/ws/XMap"); // xServer internet
-            meta.SetCredentials("xtok", "678890783139995"); // set the basic authentication properties, e.g. xtok/token for xserver internet
+            // Initialize the map
+            InsertXMapBaseLayers(map.Layers, meta);
+        }
 
-            return meta;
+        public void InsertXMapBaseLayers(LayerCollection layers, XMapMetaInfo meta)
+        {
+            InsertXMapBaseLayers(layers, meta.Url, meta.CopyrightText, meta.MaxRequestSize, meta.User, meta.Password);
         }
 
         // Initialize the xServer base map layers
-        public void InsertXMapBaseLayers(LayerCollection layers, XMapMetaInfo meta)
+        public void InsertXMapBaseLayers(LayerCollection layers, string url, string copyrightText, Size maxRequestSize, string user, string password)
         {
             var baseLayer = new TiledLayer("Background") 
             {
-                TiledProvider = new XMapTiledProvider(meta.Url, XMapMode.Background) { User = meta.User, Password = meta.Password, ContextKey = "in case of context key" },
-                Copyright = meta.CopyrightText,
+                TiledProvider = new XMapTiledProvider(url, XMapMode.Background) { User = user, Password = password, ContextKey = "in case of context key" },
+                Copyright = copyrightText,
                 Caption = MapLocalizer.GetString(MapStringId.Background),
                 IsBaseMapLayer = true,
                 Icon = ResourceHelper.LoadBitmapFromResource("Ptv.XServer.Controls.Map;component/Resources/Background.png"),
@@ -65,9 +63,9 @@ namespace CustomInit
 
             var labelLayer = new UntiledLayer("Labels")
             {
-                UntiledProvider = new XMapTiledProvider(meta.Url, XMapMode.Town) { User = meta.User, Password = meta.Password, ContextKey = "in case of context key" },
-                Copyright = meta.CopyrightText,
-                MaxRequestSize = meta.MaxRequestSize,
+                UntiledProvider = new XMapTiledProvider(url, XMapMode.Town) { User = user, Password = password, ContextKey = "in case of context key" },
+                Copyright = copyrightText,
+                MaxRequestSize = maxRequestSize,
                 Caption = MapLocalizer.GetString(MapStringId.Labels),
                 Icon = ResourceHelper.LoadBitmapFromResource("Ptv.XServer.Controls.Map;component/Resources/Labels.png"),
             };
