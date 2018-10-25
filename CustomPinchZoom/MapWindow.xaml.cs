15a00030f7d59c5cb90f9293e8d7368f48df5b8a
HolgerHermann <holger.hermann@ptvgroup.com>
2018-10-25 13:19:08 +0200

    Refactored project CustomPinchZoom without using high end compiler features.

--- CustomPinchZoom/MapWindow.xaml.cs
+++ CustomPinchZoom/MapWindow.xaml.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Diagnostics;
-using Ptv.XServer.Controls.Map;
+﻿using System.Diagnostics;
 using Ptv.XServer.Controls.Map.Tools;
 using System.Windows;
 using System.Windows.Controls;
-using Ptv.XServer.Controls.Map.Gadgets;
 
 namespace CustomPinchZoom
 {
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MapWindow : Window
+    public partial class MapWindow
     {
-        readonly PanAndZoom customPanAndZoom = new PanAndZoom();
+        private readonly PanAndZoom customPanAndZoom = new PanAndZoom();
 
         public MapWindow()
         {
@@ -22,7 +19,7 @@ namespace CustomPinchZoom
             // set map parameters
             Map.XMapStyle = "silkysand";
             Map.XMapUrl = "https://api-test.cloud.ptvgroup.com/xmap/ws/XMap";
-            Map.XMapCredentials = "xtok:<insert-your-token-here>";
+            Map.XMapCredentials = "xtok:EBB3ABF6-C1FD-4B01-9D69-349332944AD9"; // just a test-token here. Use your own token
 
             // get the map container grid
             var grid = MapElementExtensions.FindChild<Grid>(Map);
@@ -37,11 +34,10 @@ namespace CustomPinchZoom
             Map.FitInWindow = true;
 
             // check token
-            if (string.IsNullOrWhiteSpace(Map.XMapCredentials) || Map.XMapCredentials.Contains("insert"))
-            {
-                MessageBox.Show("Please adopt the Map.XMapCredentials parameter in MapWindow.xaml.cs and provide a valid token.");
-                Process.GetCurrentProcess().Kill();
-            }
+            if (!string.IsNullOrWhiteSpace(Map.XMapCredentials) && !Map.XMapCredentials.Contains("insert")) return;
+
+            MessageBox.Show("Please adopt the Map.XMapCredentials parameter in MapWindow.xaml.cs and provide a valid token.");
+            Process.GetCurrentProcess().Kill();
         }
     }
 }
