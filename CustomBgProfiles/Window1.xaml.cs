9a82bb5288dfb2eb7b563c25efa948a2990f342a
HolgerHermann <holger.hermann@ptvgroup.com>
2018-10-25 09:20:36 +0200

    Refactored project CustomBgProfiles.

--- CustomBgProfiles/Window1.xaml.cs
+++ CustomBgProfiles/Window1.xaml.cs
@@ -10,7 +10,7 @@ namespace CustomBgProfiles
     /// <summary>
     /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class Window1
     {
         public Window1()
         {
@@ -23,36 +23,26 @@ namespace CustomBgProfiles
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            switch ((sender as RadioButton).Content.ToString())
+            switch ((sender as RadioButton)?.Content.ToString())
             {
-                case "Standard":
-                    SetProfile(Map0, null);
-                    break;
-                case "Sandbox":
-                    SetProfile(Map0, "sandbox");
-                    break;
-                case "Silkysand":
-                    SetProfile(Map0, "silkysand");
-                    break;
-                default:
-                    SetProfile(Map0, "gravelpit");
-                    break;
+                case "Standard": SetProfile(Map0, null); break;
+                case "Sandbox": SetProfile(Map0, "sandbox"); break;
+                case "Silkysand": SetProfile(Map0, "silkysand"); break;
+                default: SetProfile(Map0, "gravelpit"); break;
             }
         }
 
-        private void SetProfile(Map map, string mapProfile)
+        private static void SetProfile(IMap map, string mapProfile)
         {
-            if (map.Layers["Background"] != null)
+            if (map.Layers["Background"] is TiledLayer tiledLayer)
             {
-                ((map.Layers["Background"] as TiledLayer).TiledProvider as XMapTiledProvider).CustomProfile =
-                    string.IsNullOrEmpty(mapProfile) ? null : mapProfile + "-bg";
-                (map.Layers["Background"] as TiledLayer).Refresh();
+                ((XMapTiledProvider) tiledLayer.TiledProvider).CustomProfile = string.IsNullOrEmpty(mapProfile) ? null : mapProfile + "-bg";
+                tiledLayer.Refresh();
             }
-            if (map.Layers["Labels"] != null)
+            if (map.Layers["Labels"] is UntiledLayer untiledLayer)
             {
-                ((map.Layers["Labels"] as UntiledLayer).UntiledProvider as XMapTiledProvider).CustomProfile =
-                    string.IsNullOrEmpty(mapProfile) ? null : mapProfile + "-fg";
-                (map.Layers["Labels"] as UntiledLayer).Refresh();
+                ((XMapTiledProvider) untiledLayer.UntiledProvider).CustomProfile = string.IsNullOrEmpty(mapProfile) ? null : mapProfile + "-fg";
+                untiledLayer.Refresh();
             }
         }
     }
