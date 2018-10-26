aac0e5ef0461834b28f2810a40ca58e80ee9f3e7
HolgerHermann <holger.hermann@ptvgroup.com>
2018-10-26 17:34:38 +0200

    Refactored project VdiPerformance without using high end compiler features.

--- VdiPerformance/App.xaml.cs
+++ VdiPerformance/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
 
@@ -12,14 +7,12 @@ namespace SymbolsAndLabels
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
             if (true)
                 RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
-
-            var x = RenderCapability.Tier;
         }
     }
 }
